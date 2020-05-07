5886dbae6a0cbd51948092c8d7dcdbc354176215
Yiding He <yhe@truxnow.com>
2020-05-07 10:53:35 -0400

    axios config.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,29 @@
     </div>
 </template>
 
+<script>
+import storage from './storage'
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data(){
+    return {
+      a:''
+    }
+  },
+  mounted(){
+    // storage.setItem('a',1);
+    // storage.setItem('b',{a:1});
+    // storage.setItem('c',10,'b');
+    // this.a = storage.getItem('b');
+    // storage.clear('a');
+    storage.clear('a','b');
+  }
+}
+</script>
+
 <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
 import store from './store'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import env from './env'
 
+
+// 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
+axios.defaults.baseURL = env.baseURL;
+axios.defaults.timeout = 8000;
+
+// request intercept https://github.com/axios/axios/issues/1383
+// 接口错误拦截 https://github.com/axios/axios
+axios.interceptors.response.use(function (response) {
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+  } else if (res.status == 10) {
+    window.location.href = '/#/login';
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
