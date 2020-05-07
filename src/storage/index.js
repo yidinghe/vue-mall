081a230af539ec7f24e5e828969bc0b9bbb856eb
Yiding He <yhe@truxnow.com>
2020-05-07 16:33:22 -0400

    nav header.

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,6 +1,40 @@
 const STORAGE_KEY = 'mall'
+
+const STORAGE_MODULE_USER = 'user'
+const STORAGE_KEY_USER_EMAIL = 'email'
+const STORAGE_KEY_USER_ID = 'id'
+const STORAGE_KEY_USER_ID_TOKEN = 'idToken'
+
 export default {
-    
+
+    setUserEmail(email) {
+        this.setItem(STORAGE_KEY_USER_EMAIL, email, STORAGE_MODULE_USER)
+    },
+
+    getUserEmail() {
+       return this.getItem(STORAGE_KEY_USER_EMAIL, STORAGE_MODULE_USER)
+    },
+
+    setUserId(id) {
+        this.setItem(STORAGE_KEY_USER_ID, id, STORAGE_MODULE_USER)
+    },
+
+    getUserId() {
+       return this.getItem(STORAGE_KEY_USER_ID, STORAGE_MODULE_USER)
+    },
+
+    setUserIdToken(idToken) {
+        this.setItem(STORAGE_KEY_USER_ID_TOKEN, idToken, STORAGE_MODULE_USER)
+    },
+
+    getUserIdToken() {
+       return this.getItem(STORAGE_KEY_USER_ID_TOKEN, STORAGE_MODULE_USER)
+    },
+
+    logout() {
+        window.sessionStorage.clear
+    },
+
     setItem(key, value, module_name) {
         if (module_name) {
             let val = this.getItem(module_name)
@@ -28,7 +62,7 @@ export default {
     clear(key, module_name) {
         let val = this.getStorage()
         if (module_name) {
-            if(!val[module_name]) return;
+            if (!val[module_name]) return;
             delete val[module_name][key]
         } else {
             delete val[key]
