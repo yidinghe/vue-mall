2a94df2cf371b5309b5edd5d31ca80a4d9627583
Yiding He <yhe@truxnow.com>
2020-05-07 15:41:28 -0400

    js

--- src/main.js
+++ src/main.js
@@ -8,23 +8,23 @@ import env from './env'
 
 
 // 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
-axios.defaults.baseURL = env.baseURL;
-axios.defaults.timeout = 8000;
+axios.defaults.baseURL = env.baseURL
+axios.defaults.timeout = 8000
 
 // request intercept https://github.com/axios/axios/issues/1383
 // 接口错误拦截 https://github.com/axios/axios
 axios.interceptors.response.use(function (response) {
-  let res = response.data;
+  let res = response.data
   if (res.status == 0) {
-    return res.data;
+    return res.data
   } else if (res.status == 10) {
-    window.location.href = '/#/login';
+    window.location.href = '/#/login'
   } else {
-    alert(res.msg);
+    alert(res.msg)
   }
-});
+})
 
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
--- src/storage/index.js
+++ src/storage/index.js
@@ -1,38 +1,38 @@
-const STORAGE_KEY = 'mall';
+const STORAGE_KEY = 'mall'
 export default {
     
     setItem(key, value, module_name) {
         if (module_name) {
-            let val = this.getItem(module_name);
-            val[key] = value;
-            this.setItem(module_name, val);
+            let val = this.getItem(module_name)
+            val[key] = value
+            this.setItem(module_name, val)
         } else {
-            let val = this.getStorage();
-            val[key] = value;
-            window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+            let val = this.getStorage()
+            val[key] = value
+            window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
         }
     },
 
     getItem(key, module_name) {
         if (module_name) {
-            let val = this.getItem(module_name);
-            if (val) return val[key];
+            let val = this.getItem(module_name)
+            if (val) return val[key]
         }
-        return this.getStorage()[key];
+        return this.getStorage()[key]
     },
 
     getStorage() {
-        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
     },
 
     clear(key, module_name) {
-        let val = this.getStorage();
+        let val = this.getStorage()
         if (module_name) {
-            if(!val[module_name]) return; 
-            delete val[module_name][key];
+            if(!val[module_name]) return;
+            delete val[module_name][key]
         } else {
-            delete val[key];
+            delete val[key]
         }
-        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
     }
 }
