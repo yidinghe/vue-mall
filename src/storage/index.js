5ae958914ebc539896c94a636646e8ce5d827347
Yiding He <yhe@truxnow.com>
2020-05-08 08:32:59 -0400

    header

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,39 +1,7 @@
 const STORAGE_KEY = 'mall'
 
-const STORAGE_MODULE_USER = 'user'
-const STORAGE_KEY_USER_EMAIL = 'email'
-const STORAGE_KEY_USER_ID = 'id'
-const STORAGE_KEY_USER_ID_TOKEN = 'idToken'
 
 export default {
-
-    setUserEmail(email) {
-        this.setItem(STORAGE_KEY_USER_EMAIL, email, STORAGE_MODULE_USER)
-    },
-
-    getUserEmail() {
-       return this.getItem(STORAGE_KEY_USER_EMAIL, STORAGE_MODULE_USER)
-    },
-
-    setUserId(id) {
-        this.setItem(STORAGE_KEY_USER_ID, id, STORAGE_MODULE_USER)
-    },
-
-    getUserId() {
-       return this.getItem(STORAGE_KEY_USER_ID, STORAGE_MODULE_USER)
-    },
-
-    setUserIdToken(idToken) {
-        this.setItem(STORAGE_KEY_USER_ID_TOKEN, idToken, STORAGE_MODULE_USER)
-    },
-
-    getUserIdToken() {
-       return this.getItem(STORAGE_KEY_USER_ID_TOKEN, STORAGE_MODULE_USER)
-    },
-
-    logout() {
-        window.sessionStorage.clear
-    },
 
     setItem(key, value, module_name) {
         if (module_name) {
