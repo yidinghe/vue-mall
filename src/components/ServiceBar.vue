45d9601f269417c9de7bcdbc6c699368d0bc63f7
Yiding He <yhe@truxnow.com>
2020-05-09 14:29:54 -0400

    Added service bar.

---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="service">
+      <div class="container">
+          sdasdas
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "service-bar"
+};
+</script>
+
+<style lang="scss">
+</style>
