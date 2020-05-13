f11490f2c407e53d055c740049b600640e2cef06
Yiding He <yidinghe2012@gmail.com>
2020-05-13 15:14:29 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:yidinghe/vue-mall.git master:gh-pages
+git push -f https://github.com/yidinghe/vue-mall.git master:gh-pages
 
 cd -
