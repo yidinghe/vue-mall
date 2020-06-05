adefbf6c700c08effd2c23df904739c59a02072a
Yiding He <yhe@truxnow.com>
2020-06-05 09:19:34 -0400

    Update OrderHeader.vue

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -17,20 +17,21 @@
   </div>
 </template>
 <script>
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 export default {
   name: "order-header",
   props: {
     title: String
   },
   computed: {
-    ...mapState(['username'])
+    ...mapState(["username"])
   }
 };
 </script>
 <style lang="scss">
 .order-header {
   padding: 30px 0;
+  border-bottom: 2px solid #FF6600;
   .header-logo {
     float: left;
   }
@@ -41,15 +42,17 @@ export default {
     line-height: 55px;
   }
   .title {
-    font-size: 28px;
     float: left;
-    color: #333333;
     margin-left: 54px;
+    h2 {
+      font-size: 28px;
+      color: #333333;
+    }
     span {
       font-size: 14px;
       margin-left: 17px;
       color: #999999;
-      font-weight: bold;
+      font-weight: 200;
     }
   }
   .username {
