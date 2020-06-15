c0be99738c6fca60ab05ebb933557147fa509f5b
Yiding He <yhe@truxnow.com>
2020-06-15 16:12:42 -0400

    added babel syntax plugin.

--- src/router/index.js
+++ src/router/index.js
@@ -1,84 +1,68 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../pages/home.vue'
-import Index from '../pages/index.vue'
-import Product from '../pages/product.vue'
-import Detail from '../pages/detail.vue'
-import Cart from '../pages/cart.vue'
-import Order from '../pages/order.vue'
-import OrderList from '../pages/orderList.vue'
-import OrderConfirm from '../pages/orderConfirmation.vue'
-import OrderPay from '../pages/orderPay.vue'
-import Login from '../pages/login.vue'
-import AliPay from '../pages/alipay.vue'
+import Router from 'vue-router'
+import Home from '../pages/home'
+import Index from '../pages/index'
+Vue.use(Router);
 
-Vue.use(VueRouter)
-
-  const routes = [
+export default new Router({
+  routes:[
     {
-      path: '/',
-      name: 'home',
-      component: Home,
-      redirect: '/index',
-      children: [
+      path:'/',
+      name:'home',
+      component:Home,
+      redirect:'/index',
+      children:[
         {
-          path: 'index',
+          path: '/index',
           name: 'index',
           component: Index,
-        },
-        {
-          path: 'product/:id',
+        }, {
+          path: '/product/:id',
           name: 'product',
-          component: Product,
-        },
-        {
-          path: 'detail/:id',
+          component: () => import('../pages/product.vue')
+        }, {
+          path: '/detail/:id',
           name: 'detail',
-          component:Detail,
-        }
-      ]
-    },
-    {
-      path: '/cart',
-      name: 'cart',
-      component: Cart,
-    },
-    {
-      path: '/order',
-      name: 'order',
-      component: Order,
-      children: [
-        {
-          path: 'list',
-          name: 'order-list',
-          component: OrderList,
-        },
-        {
-          path: 'confirm',
-          name: 'order-confirm',
-          component: OrderConfirm,
-        },
-        {
-          path: 'pay',
-          name: 'order-pay',
-          component: OrderPay,
-        },
-        {
-          path: 'alipay',
-          name: 'alipay',
-          component: AliPay,
+          component: () => import('../pages/detail.vue')
         }
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login,
+      component: () => import('../pages/login.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../pages/cart.vue')
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('../pages/order.vue'),
+      children:[
+        {
+          path: 'list',
+          name: 'order-list',
+          component: () => import('../pages/orderList.vue')
+        },
+        {
+          path: 'confirm',
+          name: 'order-confirm',
+          component: () => import('../pages/orderConfirmation.vue')
+        },
+        {
+          path: 'pay',
+          name: 'order-pay',
+          component: () => import('../pages/orderPay.vue')
+        },
+        {
+          path: 'alipay',
+          name: 'alipay',
+          component: () => import('../pages/alipay.vue')
+        }
+      ]
     }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
+  ]
+});
--- vue.config.js
+++ vue.config.js
@@ -15,4 +15,7 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
+  chainWebpack:(config) => {
+    config.plugins.delete('prefetch');
+  }
 }
